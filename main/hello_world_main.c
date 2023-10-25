e3fd3c997e8785a3968f9149cac7ec847822e87f
jurco-exe <jurajbosy1@gmail.com>
2023-10-25 18:37:46 +0200

    feat: first try ma nigga

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -2,9 +2,34 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
-TaskHandle_t 
+// GLOBAL Task Handles set to NULL
+TaskHandle_t firstTaskHandle = NULL;
+TaskHandle_t secondTaskHandle = NULL;
+TaskHandle_t thirdTaskHandle = NULL;
 
-void app_main(void)
-{
+void firstTask(void *arg){
+    while(1){
+        printf("Task 1 - 1000m");
+        vTaskDelay(1000 / portTICK_period_MS);
+    }
+}
 
+void secondTask(void *arg){
+    while(1){
+        printf("Task 2 - 100ms");
+        vTaskDelay(100 / portTICK_period_MS);
+    }
+}
+
+void thirdTask(void *arg){
+    while(1){
+        printf("Task 3 - 3000ms");
+        vTaskDelay(3000 / portTICK_period_MS);
+    }
+}
+
+void app_main(void){
+    xTaskCreate(firstTask, "First_Task", 4096, NULL, 1, &firstTaskHandle);
+    xTaskCreate(secondTask, "Second_Task", 4096, NULL, 2, &secondTaskHandle);
+    xTaskCreate(thirdTask, "Third_Task", 4096, NULL, 3, &thirdTaskHandle);
 }
