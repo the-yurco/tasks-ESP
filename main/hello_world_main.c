10c80d8d1a5ee466b134ea3154601317b4fe3f63
jurco-exe <jurajbosy1@gmail.com>
2023-10-25 20:05:07 +0200

    feat: full led diode done

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -1,35 +1,47 @@
 #include <stdio.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include "driver/gpio.h"
 
-// GLOBAL Task Handles set to NULL
-TaskHandle_t firstTaskHandle = NULL;
-TaskHandle_t secondTaskHandle = NULL;
-TaskHandle_t thirdTaskHandle = NULL;
+#define ESP_INR_FLAG_DEFAULT 0
+#define LED_PIN  21
+#define PUSH_BUTTON_PIN  19
 
-void firstTask(void *arg){
-    while(1){
-        printf("Task 1 - 1000m \n");
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+TaskHandle_t ButtonTask = NULL;
+TaskHandle_t LEDTask = NULL;
+
+void button_task(void *arg) {
+    while (1) {
+        if (gpio_get_level(PUSH_BUTTON_PIN) == 0) {
+            xTaskNotify(LEDTask, 0, eNoAction);
+            vTaskDelay(pdMS_TO_TICKS(100)); // Debounce delay
+            while (gpio_get_level(PUSH_BUTTON_PIN) == 0) {
+                vTaskDelay(pdMS_TO_TICKS(10)); // Wait for button release
+            }
+        }
+        vTaskDelay(pdMS_TO_TICKS(10)); // Poll the button state every 10ms
     }
 }
 
-void secondTask(void *arg){
-    while(1){
-        printf("Task 2 - 100ms \n");
-        vTaskDelay(100 / portTICK_PERIOD_MS);
+void LED_task(void *arg) {
+    bool led_status = false;
+    while (1) {
+        if (xTaskNotifyWait(0, 0, NULL, portMAX_DELAY) == pdPASS) {
+            led_status = !led_status;
+            gpio_set_level(LED_PIN, led_status);
+            printf("Button pressed for LED task!\n");
+        }
     }
 }
 
-void thirdTask(void *arg){
-    while(1){
-        printf("Task 3 - 3000ms \n");
-        vTaskDelay(3000 / portTICK_PERIOD_MS);
-    }
-}
+void app_main(void) {
+    // Configure the push button in pull-up mode
+    gpio_set_direction(PUSH_BUTTON_PIN, GPIO_MODE_INPUT);
+    gpio_set_pull_mode(PUSH_BUTTON_PIN, GPIO_PULLUP_ONLY);
 
-void app_main(void){
-    xTaskCreate(firstTask, "First_Task", 4096, NULL, 1, &firstTaskHandle);
-    xTaskCreate(secondTask, "Second_Task", 4096, NULL, 2, &secondTaskHandle);
-    xTaskCreate(thirdTask, "Third_Task", 4096, NULL, 3, &thirdTaskHandle);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_level(LED_PIN, 0); // Initialize LED as OFF
+
+    xTaskCreate(button_task, "Button_task", 4096, NULL, 2, &ButtonTask);
+    xTaskCreate(LED_task, "LED_task", 4096, NULL, 1, &LEDTask);
 }
